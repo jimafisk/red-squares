297512861a51909a0caf1df067df243d889a9c55
jimafisk <jimafisk@gmail.com>
2021-11-11 17:48:43 -0500

    One red square.

---
+++ layouts/components/squares.svelte
@@ -0,0 +1,18 @@
+<section>
+  <div>1</div>
+</section>
+
+<style>
+  section {
+    display: flex;
+  }
+  div {
+    height: 50px;
+    width: 50px;
+    margin: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: red;
+  }
+</style>
--- layouts/content/index.svelte
+++ layouts/content/index.svelte
@@ -1,8 +1,12 @@
 <script>
 	export let title;
+
+  import Squares from '../components/squares.svelte';
 </script>
 
 <h1>{title}</h1>
+
+<Squares />
 
 <style>
 	h1 {
