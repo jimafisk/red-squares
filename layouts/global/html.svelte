02d355c314e8cb8eeb070c8556dc5e85778c8b48
jimafisk <jimafisk@gmail.com>
2021-11-11 17:32:33 -0500

    Initial commit.

---
+++ layouts/global/html.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Head from './head.svelte';
+
+  export let content, layout;
+</script>
+
+<html lang="en">
+<Head title={content.filename} />
+<body>
+  <svelte:component this={layout} {...content.fields} />
+</body>
+</html>
